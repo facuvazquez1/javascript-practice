e9b6b73d78700424675e3627bbd026dafac547fe
facuvazquez1 <facugaston98@gmail.com>
2026-04-26 15:15:59 -0300

    Resolucion completa ejercicios de bucles

--- Basic/15.exercises-bucles.js
+++ Basic/15.exercises-bucles.js
@@ -35,14 +35,74 @@ for (let nombres of myArray){
 
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
 
+const myString = "Hola mi nombre es Facundo"
+let contador1 = 0
+
+for ( let vocales of myString){
+    if (vocales === "a" || vocales === "e" || vocales === "i" || vocales === "o" || vocales === "u" )
+    contador1++    
+}
+console.log(contador1)
 
 
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
 
+const myArray2 = [1, 5, 6, 10]
+let producto = 1
+
+for (let number of myArray2){
+    producto = producto * number
+    
+  
+}
+console.log(producto)
+
+
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+
+
+
+for (let i = 0; i <= 10; i++){
+    let tabla = i * 5
+    console.log(`5 x ${i} = ${tabla}`)
+
+}
+
 
 // 8. Usa un bucle para invertir una cadena de texto
 
+let cadenaTexto = "Hola mi nombre es Facundo Vazquez"
+let invertida = ""
+
+for (let i = cadenaTexto.length -1; i >= 0; i--) {
+    let letra = cadenaTexto[i]
+    invertida += letra
+}
+
+console.log(invertida)
+
 // 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
 
+let a = 0
+let b = 1
+
+for (let i = 0; i < 10; i++) {
+  console.log(a)
+  const siguiente = a + b
+  a = b
+  b = siguiente
+}
+
+
 // 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
+
+const myArray3 = [1, 4, 5, 6, 10, 12, 15, 20]
+const myArrayMayores = []
+
+for (const mayores of myArray3) {
+    if (mayores > 10) {
+        myArrayMayores.push(mayores)
+    }
+}
+
+console.log(myArrayMayores)
