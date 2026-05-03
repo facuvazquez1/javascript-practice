0802869c951808a7428e2ceade9f38cc17c5d858
facuvazquez1 <facugaston98@gmail.com>
2026-05-02 21:14:10 -0300

    Agregar ejercicios de desestructuración y propagación: extracción de datos de arrays y objetos.

--- Basic/19-exercises-objetcs.js
+++ Basic/19-exercises-objetcs.js
@@ -1,19 +1,72 @@
 // 1. Crea un objeto con 3 propiedades
 
+let auto = {
+    modelo: 205,
+    marca: "Jeep",
+    anio: 2016,
+}
+
 // 2. Accede y muestra su valor
+
+console.log(auto.modelo)
+console.log(auto.marca)
+console.log(auto.anio)
 
 // 3. Agrega una nueva propiedad
 
+auto.color = "Blanco"
+
+
 // 4. Elimina una de las 3 primeras propiedades
+
+delete auto.modelo
+console.log(auto)
 
 // 5. Agrega una función e invócala
 
+auto.seMueve = function movimiento() {
+    console.log("el auto se mueve")
+}
+
+auto.seMueve()
+
 // 6. Itera las propiedades del objeto
+
+for (let key in auto) {
+    console.log(key)
+}
+
+
 
 // 7. Crea un objeto anidado
 
+auto.motor = {
+    serial: 9092031,
+    marca: "honda",
+    anio: 2016,
+}
+
+console.log(auto)
+
 // 8. Accede y muestra el valor de las propiedades anidadas
+
+
+console.log(auto.motor.serial)
+console.log(auto.motor.marca)
+
 
 // 9. Comprueba si los dos objetos creados son iguales
 
+let moto = {
+    modelo: "honda",
+    anio: 2016,
+}
+
+moto === auto // siempre dara false porque dos objetos comparan el objeto y los objetos no pueden ser nunca iguales porque ocupan un espacio de memoria distinto.
+
+
+
 // 10. Comprueba si dos propiedades diferentes son iguales
+
+console.log(moto.anio === auto.anio) // Resultado: true 
+
---
+++ Basic/20-DesestructuracionPropagacion.js
@@ -0,0 +1,71 @@
+// Desestructuracion 
+// extraer datos de array y objetos y asignarlos a variables de manera consisa 
+
+
+// Ejemplo de como asignamos valores a una variable normalemente 
+myArray = [1, 2, 3, 4, 5]
+
+let person = {
+    name: "Facundo Vazquez",
+    age: 39,
+    email: "facu@hotmail.com"
+}
+
+myValue = myArray[1]
+console.log(myValue) // Resultado: 2
+
+let myName = person.name // Resultado: Facundo Vazquez
+
+
+// ------------------------ Sintaxis en arrays ------------------------
+
+let [myValue0, myValue1, myValue2, myValue3, myValue4, myValue5] = myArray
+console.log(myValue0) // Resultado: 1
+console.log(myValue1) // Resultado: 2
+console.log(myValue2) // Resultado: 3
+console.log(myValue3) // Resultado: 4
+console.log(myValue4) // Resultado: 5
+console.log(myValue5) // Resultado: undefined (no existe la posicion 0 en myArray)
+
+// Sintaxis con valores predeterminados
+
+let [myValue6 = 0 , myValue7 = 0, myValue8 = 0, myValue9 = 0, myValue10 = 0, myValue11 = 0] = myArray
+
+console.log(myValue6) // Resultado: 1 (aunque asignamos valor preteterminado, el array si tiene un valor para esta posicion)
+console.log(myValue7) // Resultado: 2
+console.log(myValue8) // Resultado: 3
+console.log(myValue9) // Resultado: 4
+console.log(myValue10) // Resultado: 5
+console.log(myValue11) // Resultado: 0 (porque definimos 0 como valor por defecto)
+
+// ignorar elemntos del array
+
+let [myValue12, , , ,myValue13] = myArray // los espacios vacios hacen ignorar los elemntos que se encuentren ahi 
+console.log(myValue12) // Resultado: 1
+console.log(myValue13) // Resultado: 5
+
+
+// ------------------------ Sintaxis objetos ------------------------
+
+let person1 = {
+    name: "Facundo Vazquez",
+    age: 39,
+    email: "facu@hotmail.com"
+}
+
+
+let { name, age, email } = person1
+console.log(name) // Ressultado: Facundo Vazquez
+console.log(age) // Ressultado: 39
+console.log(email) // Ressultado: facu@hotmail.com
+
+
+// Sintaxis objetos con valores predeterminados 
+
+let { name, age, email = "nuevoemail@hotmail.com"} = person1 // los key dentro de los corchetes, tiene que ser la misma que el objeto
+console.log(name)
+console.log(email) // Resultado: nuevoemail@hotmail.com (Modificamos el valor del objeto)
+
+
+
+
