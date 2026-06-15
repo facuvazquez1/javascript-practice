81dd1120543d85def49a077e032459af2e2371e8
facuvazquez1 <facugaston98@gmail.com>
2026-06-15 16:09:47 -0300

    feat(clases): agregar clases avanzadas y polimorfismo

--- 04-Intermediate/04-advanced-objects.js
+++ 04-Intermediate/04-advanced-objects.js
@@ -32,8 +32,42 @@ console.log(person)
 person.sayAge() // Rsultado: Tengo 37 anios
 
 
-// - Herencia
+// - Herencia basada en prototipos
+
+console.log("---------")
 
 let programador = Object.create(person)
+programador.lenguage = "Javascript"
+
 console.log(programador) // Resultado: {} (vacio)
-console.log(programador.greet()) // Resultado: undefined
+programador.greet()// Resultado: Hola, soy Gaston
+programador.sayAge() // Resultado: Tengo 37 anios
+
+// - Metodos estaticos y de instancia 
+
+// Antiguamente como se declaraba una clase
+ function Person (name, age) {
+    this.name = name
+    this.age = age
+ }
+
+ let new_person = new Person("Brais", 37)
+
+ Person.prototype
+
+
+ // - Metodos avanzados
+
+ // assign
+
+ let person_core = {name: "Facu"}
+ let person_details = {age: 37, alias: "facuva"}
+
+let fullPerson = Object.assign(person_core, person_details) // Combinacion de dos objetos
+console.log(fullPerson) // Resultado: { name: 'Facu', age: 37, alias: 'facuva' }
+
+// keys, values, entries
+
+console.log(Object.keys(fullPerson)) // Resultado: [ 'name', 'age', 'alias' ]
+console.log(Object.values(fullPerson)) // Resultado: [ 'Facu', 37, 'facuva' ]
+console.log(Object.entries(fullPerson)) // Resultado: [ [ 'name', 'Facu' ], [ 'age', 37 ], [ 'alias', 'facuva' ] ]
---
+++ 04-Intermediate/05-advanced-classes.js
@@ -0,0 +1,71 @@
+// Clases avanzadas
+
+class Person {
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+
+    greet() {
+        console.log(`Hola, soy ${this.name}`)
+    }
+}
+
+const person = new Person("Brais", 37)
+person.greet() // Resultado: Hola, soy Brais
+
+console.log(person)
+
+// creamos una funcion por fuera de la clase, anteriormente se usaba asi
+person.sayAge = function(){
+    console.log(`Tengo ${this.age} anios`)
+}
+
+person.sayAge() // Tengo 37 anios
+
+// Clases abstractas
+// Definicion: es una clase que no puede utilizarse como instancia directamente, si no que seran usadas por otras clases especializadas.
+
+class Animal {
+    constructor(name){
+        if (new.target === Animal) {
+            throw new Error("No se puede instanciar una clase abstracta")
+        }
+        this.name = name
+    }
+
+    makeSound(){
+        throw new Error("Este metodo tiene que ser implementado por la subclase")
+    }
+}
+
+// Demotracion del error: 
+// const animal = new Animal("Mou")
+// console.log(animal) Error: No se pued einstanciar una clase abstracta
+
+
+// Polimorfismo
+// Implementar metodos con un mismo nombre pero con comportamientos distintos
+
+class Cat extends Animal {
+    makeSound(){
+        console.log("Miau")
+    }
+
+}
+
+class Dog extends Animal {
+    makeSound(){
+        console.log("Guau")
+    }
+}
+
+const cat = new Cat("FacuCat")
+console.log(cat)
+cat.makeSound()
+
+const dog = new Dog("FacuDog")
+console.log(dog)
+dog.makeSound()
+
+// - Mixins 
