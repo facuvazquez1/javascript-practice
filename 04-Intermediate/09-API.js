5c7dcc3485bd7947f28930ca8d6036058b9db5a2
facuvazquez1 <facugaston98@gmail.com>
2026-06-21 20:24:46 -0300

    feat(api): agregar definición y manejo de APIs

---
+++ 04-Intermediate/09-API.js
@@ -0,0 +1,6 @@
+// API (Application Programming Interface)
+// Definicion: conjunto de reglas y protocolos que permiten comunicarnos entre distintas aplicaciones web. 
+
+// - Manejo de APIs 
+
+// - APIs REST (HTTP + URLs + JSON)
